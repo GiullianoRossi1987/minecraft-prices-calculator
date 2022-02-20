ea1763c3462dededbb2d3da5f161a0edcac186db
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2022-02-20 14:33:34 -0300

    Main dinamic prices model created

--- dinamic_prices/models.py
+++ dinamic_prices/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+class Sell(models.Model):
+	"""
+	"""
+	cd = models.IntegerField(primary_key=True)
+	item = models.TextField()
+	qtd = models.IntegerField()
+	value = models.DecimalField(max_digits=10, decimal_places=2)
+	datetime_sell = models.DateTimeField()
+
+
+
+
